ce61f80200a9e33ef75cccca787fb9e61eea75d0
emsthf <deunsol916@gmail.com>
2021-07-30 14:23:50 +0900

    add card and edit form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
@@ -7,6 +7,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({authService}) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1', 
+            name: 'Steve Jobs',
+            company: 'Apple', 
+            theme: 'dark', 
+            title: 'former CEO', 
+            email: 'jobs@apple.com', 
+            message: 'one more thing', 
+            fileName: 'jobs', 
+            fileURL: null
+        }, 
+        {
+            id: '2', 
+            name: 'Dongjin koh', 
+            company: 'Samsung Electronics', 
+            theme: 'light', 
+            title: 'CEO', 
+            email: 'djkoh@samsung.com', 
+            message: 'show you', 
+            fileName: 'koh', 
+            fileURL: null
+        }, 
+        {
+            id: '3', 
+            name: 'Tim Cook', 
+            company: 'Apple', 
+            theme: 'colorful', 
+            title: 'CEO', 
+            email: 'timcook@apple.com', 
+            message: 'hello', 
+            fileName: 'cook', 
+            fileURL: null
+        }, 
+    ]);
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({authService}) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer />
         </section>
